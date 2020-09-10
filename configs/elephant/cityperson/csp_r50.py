29ff86126593820604d8cb2a6c85d3c391f8869b
hasanirtiza <ihasan085@gmail.com>
2020-09-10 12:02:00 +0400

    Fixed csp file

---
+++ configs/elephant/cityperson/csp_r50.py
@@ -0,0 +1,147 @@
+# model settings
+model = dict(
+    type='CSP',
+    pretrained='modelzoo://resnet50',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        strides=(1, 2, 2, 1),
+        dilations=(1, 1, 1, 2),
+        out_indices=(1, 2, 3),
+        frozen_stages=-1,
+        norm_eval=False,
+        style='pytorch'),
+    neck=dict(
+        type='CSPNeck',
+        in_channels=[512, 1024, 2048],
+        out_channels=256,
+        start_level=0,
+        add_extra_convs=True,
+        extra_convs_on_inputs=False,  # use P5
+        num_outs=5,
+        relu_before_extra_convs=True),
+    bbox_head=dict(
+        type='CSPHead',
+        num_classes=2,
+        in_channels=768,
+        stacked_convs=1,
+        feat_channels=256,
+        strides=[4],
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=0.01),
+        loss_bbox=dict(type='IoULoss', loss_weight=1),
+        loss_offset=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=0.1)))
+# training and testing settings
+train_cfg = dict(
+    assigner=dict(
+        type='MaxIoUAssigner',
+        pos_iou_thr=0.5,
+        neg_iou_thr=0.4,
+        min_pos_iou=0,
+        ignore_iof_thr=-1),
+    allowed_border=-1,
+    pos_weight=-1,
+    debug=False)
+test_cfg = dict(
+    nms_pre=1000,
+    min_bbox_size=0,
+    score_thr=0.1, #0.2, #0.05,
+    nms=dict(type='nms', iou_thr=0.5),
+    max_per_img=100)
+# dataset settings
+dataset_type = 'CocoCSPORIDataset'
+data_root = 'datasets/CityPersons/'
+INF = 1e8
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+data = dict(
+    imgs_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'leftImg8bit_trainvaltest/train.json',
+        img_prefix=data_root,
+
+        img_scale=(1280, 640),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=128,
+        flip_ratio=0.5,
+        with_mask=False,
+        with_crowd=True,
+        with_label=True,
+        remove_small_box=True,
+        small_box_size=8,
+        strides=[4],
+        regress_ranges=((-1, INF),)),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'annotations/val.json',
+        img_prefix=data_root + 'val_data/',
+        #img_scale=(1333, 800),
+        img_scale = (800, 600),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=32,
+        flip_ratio=0,
+        with_mask=False,
+        with_crowd=False,
+        with_label=True),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + '/leftImg8bit_trainvaltest/val_gt_for_mmdetction.json',
+        img_prefix=data_root + '/leftImg8bit_trainvaltest/leftImg8bit/val_all_in_folder/',
+        img_scale=(2048, 1024),
+        img_norm_cfg=img_norm_cfg,
+        size_divisor=128,
+        flip_ratio=0,
+        with_mask=False,
+        with_crowd=False,
+        with_label=False,
+        test_mode=True))
+# optimizer
+# optimizer = dict(
+#     type='SGD',
+#     lr=0.01/10,
+#     momentum=0.9,
+#     weight_decay=0.0001,
+#     paramwise_options=dict(bias_lr_mult=2., bias_decay_mult=0.))
+mean_teacher = True
+optimizer = dict(
+    type='Adam',
+    lr=2e-4,
+)
+# optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = dict(mean_teacher = dict(alpha=0.999))
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='constant',
+    warmup_iters=500,
+    warmup_ratio=1.0 / 3,
+    step=[110, 160])
+
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+# runtime settings
+total_epochs = 240
+device_ids = range(8)
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+work_dir = './work_dirs/ori_csp'
+load_from = None
+# load_from = '/home/ljp/code/mmdetection/work_dirs/fcos_mstrain_640_800_x101_64x4d_fpn_gn_2x/epoch_22.pth'
+resume_from = None
+# resume_from = '/home/ljp/code/mmdetection/work_dirs/csp4_mstrain_640_800_x101_64x4d_fpn_gn_2x/epoch_10.pth'
+workflow = [('train', 1)]
