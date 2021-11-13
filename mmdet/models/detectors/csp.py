c798c028e98ddb7a8442a971670e1315bb0dc215
Abdul Hannan Khan <khan.abdulhannan777@gmail.com>
2021-11-13 14:34:38 +0100

    Added optional refine head to CSP

--- mmdet/models/detectors/csp.py
+++ mmdet/models/detectors/csp.py
@@ -1,9 +1,12 @@
 from .single_stage import SingleStageDetector
 from ..registry import DETECTORS
 from mmdet.core import bbox2result
-
+import torch.nn as nn
+import torch
+from .. import builder
 import numpy as np
 import cv2
+from mmdet.core import bbox2roi, bbox2result, build_assigner, build_sampler
 
 @DETECTORS.register_module
 class CSP(SingleStageDetector):
@@ -12,13 +15,22 @@ class CSP(SingleStageDetector):
                  backbone,
                  neck,
                  bbox_head,
+                 refine_roi_extractor=None,
+                 refine_head=None,
                  train_cfg=None,
                  test_cfg=None,
                  pretrained=None,
                  return_feature_maps=False):
         super(CSP, self).__init__(backbone, neck, bbox_head, train_cfg,
                                    test_cfg, pretrained)
+        if refine_head is not None:
+            self.bbox_roi_extractor = builder.build_roi_extractor(
+                refine_roi_extractor)
+            self.refine_head = builder.build_head(refine_head)
         self.return_feature_maps = return_feature_maps
+        self.train_cfg = train_cfg
+        self.test_cfg = test_cfg
+
     def show_input_debug(self, img, classification_maps, scale_maps, offset_maps):
         img_numpy = img.cpu().numpy().copy()[0]
         # img_numpy = np.transpose(img_numpy, [1, 2, 0]) * [58.395, 57.12, 57.375] + [123.675, 116.28, 103.53]
@@ -168,6 +180,10 @@ class CSP(SingleStageDetector):
                 cv2.imshow(str(i) +'img', img_now)
             cv2.waitKey(0)
 
+    @property
+    def refine(self):
+        return hasattr(self, 'refine_head') and self.refine_head is not None
+
     def forward_train(self,
                       img,
                       img_metas,
@@ -188,6 +204,8 @@ class CSP(SingleStageDetector):
             scale_maps = scale_maps[0]
             offset_maps = offset_maps[0]
 
+        losses = dict()
+
         x = self.extract_feat(img)
         # self.show_input_debug(img, classification_maps, scale_maps, offset_maps)
         # self.show_input_debug_caltech(img, classification_maps, scale_maps, offset_maps)
@@ -195,8 +213,45 @@ class CSP(SingleStageDetector):
         # self.show_input_debug_head(img, classification_maps, scale_maps, offset_maps)
         outs = self.bbox_head(x)
         loss_inputs = outs + (gt_bboxes, gt_labels, classification_maps, scale_maps, offset_maps, img_metas, self.train_cfg)
-        losses = self.bbox_head.loss(
+        losses_bbox = self.bbox_head.loss(
             *loss_inputs, gt_bboxes_ignore=gt_bboxes_ignore)
+        losses.update(losses_bbox)
+
+        bbox_inputs = outs + (img_metas, self.test_cfg, True)
+        bbox_list = self.bbox_head.get_bboxes(*bbox_inputs)
+
+        if self.refine:
+            bbox_assigner = build_assigner(self.train_cfg.rcnn.assigner)
+            bbox_sampler = build_sampler(
+                self.train_cfg.rcnn.sampler, context=self)
+            num_imgs = img.size(0)
+            if gt_bboxes_ignore is None:
+                gt_bboxes_ignore = [None for _ in range(num_imgs)]
+            sampling_results = []
+            for i in range(num_imgs):
+                assign_result = bbox_assigner.assign(
+                    bbox_list[i], gt_bboxes[i], gt_bboxes_ignore[i],
+                    gt_labels[i])
+                sampling_result = bbox_sampler.sample(
+                    assign_result,
+                    bbox_list[i],
+                    gt_bboxes[i],
+                    gt_labels[i],
+                    feats=[lvl_feat[i][None] for lvl_feat in x])
+                sampling_results.append(sampling_result)
+
+            rois = bbox2roi([res.bboxes for res in sampling_results])
+            # TODO: a more flexible way to decide which feature maps to use
+            bbox_feats = self.refine_roi_extractor(
+                x[:self.refine_roi_extractor.num_inputs], rois)
+            cls_score, bbox_pred = self.refine_head(bbox_feats)
+
+            bbox_targets = self.refine_head.get_target(
+                sampling_results, gt_bboxes, gt_labels, self.train_cfg.rcnn)
+            loss_refine = self.refine_head.loss(cls_score, bbox_pred,
+                                            *bbox_targets)
+            losses.update(dict(loss_refine_cls=loss_refine["loss_cls"], acc=loss_refine["acc"]))
+
         return losses
 
     def simple_test(self, img, img_meta, rescale=False):
@@ -206,6 +261,24 @@ class CSP(SingleStageDetector):
         if self.return_feature_maps:
             return self.bbox_head.get_bboxes_features(*bbox_inputs)
         bbox_list = self.bbox_head.get_bboxes(*bbox_inputs)
+        if self.refine:
+
+            rois = bbox2roi(bbox_list)
+            roi_feats = self.refine_roi_extractor(
+                x[:len(self.refine_roi_extractor.featmap_strides)], rois)
+            cls_score, bbox_pred = self.refine_head(roi_feats)
+            img_shape = img_meta[0]['img_shape']
+            scale_factor = img_meta[0]['scale_factor']
+            det_bboxes, det_labels = self.refine_head.get_det_bboxes(
+                rois,
+                cls_score,
+                bbox_pred,
+                img_shape,
+                scale_factor,
+                rescale=False,
+                cfg=self.test_cfg)
+            return bbox2result(det_bboxes, det_labels, self.refine_head.num_classes)
+
         bbox_results = [
             bbox2result(det_bboxes, det_labels, self.bbox_head.num_classes)
             for det_bboxes, det_labels in bbox_list
