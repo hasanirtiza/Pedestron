4c760c3cd396d92ee134dd3077aab06f65b85961
Abdul Hannan Khan <khan.abdulhannan777@gmail.com>
2021-12-06 23:12:57 +0100

    Added CSP Transformer Head

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -1,3 +1,4 @@
 from .registry import Registry, build_from_cfg
+from .logger import get_root_logger
 
-__all__ = ['Registry', 'build_from_cfg']
+__all__ = ['Registry', 'build_from_cfg', 'get_root_logger']
