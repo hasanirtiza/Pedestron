d10e362749fb17dba81bb2551ce7f8df2b144625
abdulhannankhan <khan.abdulhannan777@gmail.com>
2022-02-14 08:53:34 +0100

    Added configuration for score combination strategy and dropout probability, fixed datatype bugs

--- mmdet/models/bbox_heads/refine_head.py
+++ mmdet/models/bbox_heads/refine_head.py
@@ -6,6 +6,7 @@ from ..utils import ConvModule
 import torch.nn.functional as F
 from ..losses import accuracy
 import torch
+from ..builder import build_loss
 from mmdet.core import (delta2bbox, multiclass_nms, bbox_target, force_fp32, bbox2result,
                         auto_fp16)
 
@@ -18,18 +19,24 @@ class RefineHead(BBoxHead):
     def __init__(self,
                  num_cls_convs=2,
                  num_cls_fcs=2,
+                 use_gm=True,
                  conv_out_channels=256,
                  fc_out_channels=1024,
                  conv_cfg=None,
                  norm_cfg=None,
+                 loss_opinion=None,
                  alpha=0.5,
+                 num_classes=1,
                  weight_decay=0.0005,
+                 dropout_prob=-1,
                  *args,
                  **kwargs):
-        super(RefineHead, self).__init__(*args, num_classes=2, with_reg=False, **kwargs)
+        super(RefineHead, self).__init__(*args, num_classes=num_classes, with_reg=False, **kwargs)
         assert (num_cls_convs + num_cls_fcs > 0)
 
         self.alpha = alpha
+        self.use_gm = use_gm
+        self.dropout_prob = dropout_prob
         self.reg_lambda = weight_decay
         self.num_cls_convs = num_cls_convs
         self.num_cls_fcs = num_cls_fcs
@@ -37,7 +44,7 @@ class RefineHead(BBoxHead):
         self.fc_out_channels = fc_out_channels
         self.conv_cfg = conv_cfg
         self.norm_cfg = norm_cfg
-
+        self.loss_opinion = build_loss(loss_opinion) if loss_opinion else loss_opinion
         # add cls specific branch
         self.cls_convs, self.cls_fcs, self.cls_last_dim = \
             self._add_conv_fc_branch(
@@ -45,7 +52,7 @@ class RefineHead(BBoxHead):
 
         self.relu = nn.LeakyReLU(0.2, inplace=True)
         # reconstruct fc_cls and fc_reg since input channels are changed
-        self.fc_cls = nn.Linear(self.cls_last_dim, 2)
+        self.fc_cls = nn.Linear(self.cls_last_dim, self.num_classes)
 
     def _add_conv_fc_branch(self,
                             num_branch_convs,
@@ -81,7 +88,8 @@ class RefineHead(BBoxHead):
                     last_layer_dim if i == 0 else self.fc_out_channels)
                 branch_fcs.append(
                     nn.Linear(fc_in_channels, self.fc_out_channels))
-                branch_fcs.append(nn.Dropout(0.5))
+                if self.dropout_prob > -1:
+                    branch_fcs.append(nn.Dropout(self.dropout_prob))
             last_layer_dim = self.fc_out_channels
         return branch_convs, branch_fcs, last_layer_dim
 
@@ -92,6 +100,14 @@ class RefineHead(BBoxHead):
                 if isinstance(m, nn.Linear):
                     nn.init.xavier_uniform_(m.weight)
                     nn.init.constant_(m.bias, 0)
+    def compute_opinion_loss(self, pred, refine):
+        loss = torch.tensor(0.0).float().cuda()
+        if self.loss_opinion is not None:
+            loss = self.loss_opinion(
+                pred,
+                refine,
+                reduction_override='mean')
+        return dict(loss_opinion=loss)
 
     def loss(self,
              cls_score,
@@ -102,8 +118,8 @@ class RefineHead(BBoxHead):
         if cls_score is not None:
             avg_factor = max(torch.sum(label_weights > 0).float().item(), 1.)
             losses['loss_cls'] = self.loss_cls(
-                cls_score,
-                labels,
+                cls_score.double(),
+                labels.double(),
                 label_weights,
                 avg_factor=avg_factor,
                 reduction_override=reduction_override)
@@ -111,7 +127,7 @@ class RefineHead(BBoxHead):
             for param in self.parameters():
                 reg_loss += param.square().sum()
             losses['loss_cls'] += self.reg_lambda * reg_loss
-            losses['acc'] = accuracy(cls_score, labels)
+            losses['dist'] = nn.L1Loss(reduction='mean')(cls_score, labels)
         return losses
 
     def forward(self, x):
@@ -125,12 +141,16 @@ class RefineHead(BBoxHead):
             x_cls = self.relu(fc(x_cls))
 
         cls_score = self.fc_cls(x_cls)
-        return cls_score
+        if self.num_classes > 1:
+            return cls_score
+        return cls_score.view(-1)
 
     def get_scores(self, x):
 
         cls_score = self.forward(x)
-        return F.softmax(cls_score, dim=1)
+        if cls_score.dim() > 1:
+            return F.softmax(cls_score, dim=1)
+        return F.sigmoid(cls_score)
 
     def suppress_boxes(self, rois, scores, img_meta=None, cfg=None):
         img_shape = img_meta[0]['img_shape']
@@ -145,7 +165,12 @@ class RefineHead(BBoxHead):
 
     def combine_scores(self, results, scores):
         results = results[0]
-        results[:, 4] *= self.alpha
-        results[:, 4] += (1.0 - self.alpha) * scores[:, 1]
+        if scores.dim() > 1:
+            scores = scores[:, -1]
+        if not self.use_gm:
+            results[:, 4] *= self.alpha
+            results[:, 4] += (1.0 - self.alpha) * scores
+        else:
+            results[:, 4] *= scores
         return [results.cpu().numpy()]
 
