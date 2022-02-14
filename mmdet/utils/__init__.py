f4f244b54a19004f5b4c6a6a720035380bb31a28
abdulhannankhan <khan.abdulhannan777@gmail.com>
2022-02-14 08:38:55 +0100

    removed wandb log

--- mmdet/utils/__init__.py
+++ mmdet/utils/__init__.py
@@ -1,4 +1,3 @@
 from .registry import Registry, build_from_cfg
-from .logger import get_root_logger
 
-__all__ = ['Registry', 'build_from_cfg', 'get_root_logger']
+__all__ = ['Registry', 'build_from_cfg']
--- mmdet/utils/logger.py
+++ mmdet/utils/logger.py
@@ -4,49 +4,16 @@ import sys
 from collections import Counter
 from typing import Tuple
 
+import torch
 import mmcv
 import numpy as np
 import cv2
 import torch
-from mmcv.runner.dist_utils import get_dist_info
-from mmcv.utils import get_logger
-from mmdet.core.visualization import imshow_det_bboxes
 
 try:
     import wandb
 except:
     wandb = None
-
-_log_counter = Counter()
-
-
-def get_root_logger(log_file=None, log_level=logging.INFO):
-    """Get root logger.
-
-    Args:
-        log_file (str, optional): File path of log. Defaults to None.
-        log_level (int, optional): The level of logger.
-            Defaults to logging.INFO.
-
-    Returns:
-        :obj:`logging.Logger`: The obtained logger
-    """
-    logger = get_logger(name="mmdet.ssod", log_file=log_file, log_level=log_level)
-    logger.propagate = False
-    return logger
-
-
-def _find_caller():
-    frame = sys._getframe(2)
-    while frame:
-        code = frame.f_code
-        if os.path.join("utils", "logger.") not in code.co_filename:
-            mod_name = frame.f_globals["__name__"]
-            if mod_name == "__main__":
-                mod_name = r"ssod"
-            return mod_name, (code.co_filename, frame.f_lineno, code.co_name)
-        frame = frame.f_back
-
 
 def convert_box(tag, boxes, box_labels, class_labels, std, scores=None):
     wandb_box = {}
@@ -103,10 +70,9 @@ def log_image_with_boxes(
     backend: str = "auto",
     interval: int = 50,
 ):
-    rank, _ = get_dist_info()
+    rank = torch.distributed.get_rank(group=None)
     if rank != 0:
         return
-    _, key = _find_caller()
     #_log_counter[key] += 1
     #if not (interval == 1 or _log_counter[key] % interval == 1):
     #    return
@@ -142,21 +108,3 @@ def log_image_with_boxes(
         bbox_tag, bboxes, labels, class_names, scores=scores, std=im_shape
     )
     wandb.log({tag: wandb.Image(**im)}, commit=False)
-
-
-def log_every_n(msg: str, n: int = 50, level: int = logging.DEBUG, backend="auto"):
-    """
-    Args:
-        msg (Any):
-        n (int):
-        level (int):
-        name (str):
-    """
-    caller_module, key = _find_caller()
-    _log_counter[key] += 1
-    return
-    if n == 1 or _log_counter[key] % n == 1:
-        if isinstance(msg, dict) and (wandb is not None) and (wandb.run is not None):
-            wandb.log(msg, commit=False)
-        else:
-            get_root_logger().log(level, msg)
