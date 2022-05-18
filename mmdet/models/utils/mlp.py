83036d4855a13ade3c48ef36ac94a8485949cf6e
Abdul Hannan Khan <khan.abdulhannan777@gmail.com>
2022-05-18 13:53:31 +0200

    Fixed Height-Width swap bug

--- mmdet/models/utils/mlp.py
+++ mmdet/models/utils/mlp.py
@@ -4,7 +4,7 @@ from torch import nn
 def window_partition(x, window_size, channel_last=True):
     """
     Args:
-        x: (B, H, W, C)
+        x: (B, W, H, C)
         window_size (int): window size
     Returns:
         windows: (B, num_windows, window_size * window_size, C)
@@ -12,25 +12,25 @@ def window_partition(x, window_size, channel_last=True):
     """
     if not channel_last:
         x = x.permute(0, 2, 3, 1)
-    B, H, W, C = x.shape
-    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    B, W, H, C = x.shape
+    x = x.view(B, W // window_size, window_size, H // window_size, window_size, C)
     windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, -1, window_size * window_size, C)
     return windows
 
 
-def window_reverse(windows, window_size, H, W):
+def window_reverse(windows, window_size, W, H):
     """
     Args:
         windows: (B, num_windows, window_size*window_size, C)
         window_size (int): Window size
-        H (int): Height of image
         W (int): Width of image
+        H (int): Height of image
     Returns:
-        x: (B, C, H, W)
+        x: (B, C, W, H)
     """
     B = windows.shape[0]
-    x = windows.reshape(B, H // window_size, W // window_size, window_size, window_size, -1)
-    x = x.permute(0, 5, 1, 3, 2, 4).contiguous().view(B, -1, H, W)
+    x = windows.reshape(B, W // window_size, H // window_size, window_size, window_size, -1)
+    x = x.permute(0, 5, 1, 3, 2, 4).contiguous().view(B, -1, W, H)
     return x
 
 
